ac4ac443987492f99bbbee0baf8a9db5b04bd8ac
Nate J <jamis.walsh@gmail.com>
2021-03-09 20:12:48 -0800

    route files

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
 
 const app = express();
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(express.urlencoded({ extended: true}));
+// app.use(express.json(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`);
+});
