e8bd01e1d97945e325e7d9a2daea530907de4051
Nate J <jamis.walsh@gmail.com>
2021-03-11 19:15:21 -0800

    one more time...

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-// app.use(express.static('public'));
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true}));
-app.use(express.json(path.join(__dirname, 'public')));
+app.use(express.json());
 
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
