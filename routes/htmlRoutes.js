378e4d37a3f3a9890e904cc78a6ea379a30a953a
Nate J <jamis.walsh@gmail.com>
2021-03-11 20:00:44 -0800

    changed htmlroute

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,19 +1,27 @@
 const path = require('path');
+const router = express.Router();
 
-module.exports = (app) => {
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, '..public/index.html'));
-    });
-    app.get('/notes', (req, res) => {
-        res.sendFile(path.join(__dirname, '..public/notes.html'));
-    });
-    app.get('/assets/css/style.css', (req, res) => {
-        res.sendFile(path.join(__dirname, '..public/assets/css/style.css'));
-    });
-    app.get('/assets/js/index.js', (req, res) => {
-        res.sendFile(path.join(__dirname, '..public/assets/js/index.js'));
-    });
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '..public/index.html'));
-    });
-}
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+module.exports = router;
+
+
+// module.exports = (app) => {
+//     app.get('/notes', (req, res) => {
+//         res.sendFile(path.join(__dirname, '..public/notes.html'));
+//     });
+//     app.get('/assets/css/style.css', (req, res) => {
+//         res.sendFile(path.join(__dirname, '..public/assets/css/style.css'));
+//     });
+//     app.get('/assets/js/index.js', (req, res) => {
+//         res.sendFile(path.join(__dirname, '..public/assets/js/index.js'));
+//     });
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.join(__dirname, '..public/index.html'));
+//     });
+// }
