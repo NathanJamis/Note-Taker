eb778b9c9f38c6b3f669085f4964ba6a6c82cdbc
Nate J <jamis.walsh@gmail.com>
2021-03-11 19:09:31 -0800

    hopefully fixed a goof

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
-
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -12,6 +9,9 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json(path.join(__dirname, 'public')));
+
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
