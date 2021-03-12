85947fb189444a453c36a706fdb0550ef7fd512a
Nate J <jamis.walsh@gmail.com>
2021-03-11 18:41:22 -0800

    finished api routes

--- server.js
+++ server.js
@@ -6,11 +6,9 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use(express.urlencoded({ extended: true}));
-// app.use(express.json(path.join(__dirname, 'public')));
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
