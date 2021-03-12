de989520362dc748d9cf48a71ecfadd0e5ac0203
Nate J <jamis.walsh@gmail.com>
2021-03-11 20:18:23 -0800

    whoops

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,15 +5,15 @@ module.exports = (app) => {
     //     res.sendFile(path.join(__dirname, '..public/index.html'));
     // });
     app.get('/notes', (req, res) => {
-        res.sendFile(path.join(__dirname, '..public/notes.html'));
+        res.sendFile(path.join(__dirname, '../public/notes.html'));
     });
     app.get('/assets/css/style.css', (req, res) => {
-        res.sendFile(path.join(__dirname, '..public/assets/css/style.css'));
+        res.sendFile(path.join(__dirname, '../public/assets/css/style.css'));
     });
     app.get('/assets/js/index.js', (req, res) => {
-        res.sendFile(path.join(__dirname, '..public/assets/js/index.js'));
+        res.sendFile(path.join(__dirname, '../public/assets/js/index.js'));
     });
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '..public/index.html'));
+        res.sendFile(path.join(__dirname, '../public/index.html'));
     });
 }
